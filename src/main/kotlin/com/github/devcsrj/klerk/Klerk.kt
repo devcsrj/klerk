bc77c49a3f3f428798db8a4894f181dc098bbe62
Reijhanniel Jearl Campos <devcsrj@apache.org>
2019-09-19 06:02:14 +0800

    🔥 Remove all references to spring batch

--- src/main/kotlin/com/github/devcsrj/klerk/Klerk.kt
+++ src/main/kotlin/com/github/devcsrj/klerk/Klerk.kt
@@ -1,20 +1,5 @@
 package com.github.devcsrj.klerk
 
-import org.springframework.boot.SpringApplication
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
-
-@SpringBootApplication
-class Klerk {
-
-    fun main(args: Array<String>) {
-        SpringApplication.run(Klerk::class.java, *args)
-    }
-
-    @Bean
-    fun listOfCongress(): List<Congress> {
-        return listOf(
-            Congress(18)
-        )
-    }
+fun main(args: Array<String>) {
+    TODO()
 }
