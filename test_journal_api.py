2eaff4f2e8f91261fd2a5dab11a04234d9fededd
Reijhanniel Jearl Campos <devcsrj@apache.org>
2019-10-06 06:20:19 +0800

    Revert "💥 Migrate from httpretty to responses"
    This reverts commit e0a3c6a037a2f651a1c617004f198447e82c0343.

--- test_journal_api.py
+++ test_journal_api.py
@@ -1,6 +1,6 @@
 import datetime
 
-import responses
+import httpretty
 
 from chamber import Chamber
 from congress import Congress
@@ -9,74 +9,77 @@ from journal_api import HouseJournalApi
 from session import Session
 
 
-@responses.activate
-def test_house_fetch_one():
-    responses.add(
-        method=responses.GET,
-        url="http://www.congress.gov.ph"
-            "/legisdocs?v=journals&congress=17&session=1",
-        body=open("test/resources/house/journal/17th-session1.html", "rb"),
-        match_querystring=True)
+class TestJournalApi:
 
-    congress = Congress(17)
-    session = Session(1, "regular")
+    @httpretty.activate
+    def test_house_fetch_one(self):
+        httpretty.register_uri(
+            method=httpretty.GET,
+            uri="http://www.congress.gov.ph/legisdocs/?v=journals",
+            body=open("test/resources/17th-session1.html", "r").read())
 
-    api = HouseJournalApi()
-    journals = api.fetch(congress, session)
+        congress = Congress(17)
+        session = Session(1, "regular")
 
-    actual = next(journals)
-    assert actual == Journal(
-        chamber=Chamber.HOUSE,
-        congress=congress,
-        session=session,
-        number=1,
-        date=datetime.date(2016, 7, 25),
-        document_uri="http://www.congress.gov.ph/legisdocs/journals_17"
-                     "/J1-1RS-20160725.pdf"
-    )
+        api = HouseJournalApi()
+        journals = api.fetch(congress, session)
 
+        actual = next(journals)
+        assert actual == Journal(
+            chamber=Chamber.HOUSE,
+            congress=congress,
+            session=session,
+            number=1,
+            date=datetime.date(2016, 7, 25),
+            document_uri="http://www.congress.gov.ph/legisdocs/journals_17"
+                         "/J1-1RS-20160725.pdf"
+        )
 
-@responses.activate
-def test_house_fetch_all():
-    responses.add(
-        method=responses.GET,
-        url="http://www.congress.gov.ph"
-            "/legisdocs?v=journals&congress=17&session=1",
-        body=open("test/resources/house/journal/17th-session1.html", "rb"))
+        rr = httpretty.last_request()
+        assert rr.path.startswith("/legisdocs")
+        query_map = rr.querystring
+        assert query_map["congress"][0] == "17"
+        assert query_map["session"][0] == "1"
+        assert query_map["v"][0] == "journals"
 
-    congress = Congress(17)
-    session = Session(1, "regular")
+    @httpretty.activate
+    def test_house_fetch_all(self):
+        httpretty.register_uri(
+            method=httpretty.GET,
+            uri="http://www.congress.gov.ph/legisdocs/?v=journals",
+            body=open("test/resources/17th-session1.html", "r").read())
 
-    api = HouseJournalApi()
-    journals = api.fetch(congress, session)
-    total = 0
-    for journal in journals:
-        total += 1
+        congress = Congress(17)
+        session = Session(1, "regular")
 
-    assert total == 97
+        api = HouseJournalApi()
+        journals = api.fetch(congress, session)
+        total = 0
+        for journal in journals:
+            total += 1
 
+        assert total == 97
 
-@responses.activate
-def test_house_fetch_with_offset():
-    responses.add(
-        method=responses.GET,
-        url="http://www.congress.gov.ph"
-            "/legisdocs?v=journals&congress=17&session=1",
-        body=open("test/resources/house/journal/17th-session1.html", "rb"))
+    @httpretty.activate
+    def test_house_fetch_with_offset(self):
+        httpretty.register_uri(
+            method=httpretty.GET,
+            uri="http://www.congress.gov.ph/legisdocs/?v=journals",
+            body=open("test/resources/17th-session1.html", "r").read())
 
-    congress = Congress(17)
-    session = Session(1, "regular")
+        congress = Congress(17)
+        session = Session(1, "regular")
 
-    api = HouseJournalApi()
-    journals = api.fetch(congress, session, offset=5)
-    actual = next(journals)
+        api = HouseJournalApi()
+        journals = api.fetch(congress, session, offset=5)
+        actual = next(journals)
 
-    assert actual == Journal(
-        chamber=Chamber.HOUSE,
-        congress=congress,
-        session=session,
-        number=6,
-        date=datetime.date(2016, 8, 3),
-        document_uri="http://www.congress.gov.ph/legisdocs/journals_17"
-                     "/J6-1RS-20160803.pdf"
-    )
+        assert actual == Journal(
+            chamber=Chamber.HOUSE,
+            congress=congress,
+            session=session,
+            number=6,
+            date=datetime.date(2016, 8, 3),
+            document_uri="http://www.congress.gov.ph/legisdocs/journals_17"
+                         "/J6-1RS-20160803.pdf"
+        )
