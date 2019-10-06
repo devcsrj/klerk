425854911ed7ce480cdc8c01faadecb0391d5631
Vince Aldrin Cabrera <vincealdrincabrera@gmail.com>
2019-10-06 21:52:45 +0800

    Add license headers

--- src/test/kotlin/com/github/devcsrj/senate/SenateHttpLegislatorApiTest.kt
+++ src/test/kotlin/com/github/devcsrj/senate/SenateHttpLegislatorApiTest.kt
@@ -1,3 +1,20 @@
+/**
+ * Klerk
+ * Copyright (C) 2019 Reijhanniel Jearl Campos
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.devcsrj.senate
 
 import com.github.devcsrj.klerk.Legislator
@@ -24,8 +41,8 @@ object SenateHttpLegislatorApiTest : Spek({
         beforeGroup {
             server = MockWebServer()
             api = SenateHttpLegislatorApi(server.url("/").toUri())
-            val json = this::class.java.getResource( "$baseDir/ph-senator-list-oct-6-2019.json")
-            expected = ObjectMapper().readValue(json, object : TypeReference<Set<Legislator>>(){})
+            val json = this::class.java.getResource("$baseDir/ph-senator-list-oct-6-2019.json")
+            expected = ObjectMapper().readValue(json, object : TypeReference<Set<Legislator>>() {})
         }
 
         afterGroup {
