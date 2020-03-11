96e926de0ef702f9489d1bd346a63ef268893f8e
Reijhanniel Jearl Campos <devcsrj@apache.org>
2020-03-11 14:36:46 +0800

    :page_facing_up: update license copyright date

--- src/main/kotlin/com/github/devcsrj/klerk/KlerkConverters.kt
+++ src/main/kotlin/com/github/devcsrj/klerk/KlerkConverters.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright [2019] [Reijhanniel Jearl Campos]
+ * Copyright [2020] [Reijhanniel Jearl Campos]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
